da19f9e8101e2ce2b4ca57fc00356ada7081fc4d
cfrancoi <corentinfrancois31@gmail.com>
2023-06-07 15:04:58 +0200

    wip - add cost function

--- srcs/learn.py
+++ srcs/learn.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from utils import utils
 
-learningReate: float = 1e-11
+learningRate = float(0.00025)
 THETA_FILE = "theta.csv"
 
 #move to utils
@@ -25,6 +25,17 @@ def get_last_theta() -> [float, float]:
     finally:
         return [th0, th1]
 
+def get_cost(X, Y, th0, th1, M) -> float:
+    value= 0
+    for i in range(M):
+        value += float(utils.estimate_price(th0, th1, X[i]) - Y[i])**2
+    
+    cost = (1/2*M) * (value)
+
+    return cost
+
+    
+
 
 def get_data() -> [[], []]:
     X, Y = [], []
@@ -42,27 +53,37 @@ def get_data() -> [[], []]:
     return [X, Y]
 
 def gradient_descent(X, Y, M, th0, th1):
-    tmp0, tmp1 = 0, 0
-    for i in range(0, M - 1):
-        tmp0 += utils.estimate_price(th0, th1, X[i]) - Y[i]
-        tmp1 += (utils.estimate_price(th0, th1, X[i]) - Y[i]) * X[i]
+    tmp0 = float(0)
+    tmp1 = float(0)
+    for i in range(M):
+        value = utils.estimate_price(th0, th1, X[i])
+        tmp0 += float(value - Y[i])
+        tmp1 += float((value - Y[i]) * X[i])
+        print("=== {th0} {th1} {value} X={x} Y={y} ===".format(th0=tmp0, th1=tmp1, value=value, x=X[i], y=Y[i]))
+        
 
-    tmp0 = tmp0 * learningReate * float(1/M)
-    tmp1 = tmp1 * learningReate * float(1/M)
+    tmp0 = float(learningRate * tmp0 * float(1/M) )
+    tmp1 = float(learningRate * tmp1 * float(1/M))
+
+    print("===  LEARN: {th0} {th1} ===".format(th0=tmp0, th1=tmp1))
+    
 
     return [tmp0, tmp1]
 
 def get_new_theta(X, Y, M, th0, th1, rep):
-    for i in range(0, rep - 1):
+    cost = []
+    for _ in range(0, rep):
         [tmp0, tmp1] = gradient_descent(X, Y, M, th0, th1)
-        th0 = th0 - tmp0;
-        th1 = th1 - tmp1;
-        # print("last theta: {th0} {th1}".format(th0=th0, th1=th1))
+        th0 = th0 - tmp0
+        th1 = th1 - tmp1
+        cost.append(get_cost(X, Y, th0, th1, M))
+        print("last theta: {th0} {th1} ".format(th0=th0, th1=th1))
     
-    return [th0, th1]
+    return [th0, th1, cost]
 
 def main() -> int:
     [X, Y] = get_data()
+
     M = len(X)
 
     [th0, th1] = get_last_theta()
@@ -70,14 +91,23 @@ def main() -> int:
     print(M)
     print("last theta: {th0} {th1}".format(th0=th0, th1=th1))
 
-    [new_th0, new_th1] = get_new_theta(X, Y, M, th0, th1, 100000)
+    [new_th0, new_th1, cost] = get_new_theta(X, Y, M, th0, th1, 5)
 
     
     print("new theta: {th0} {th1}".format(th0=new_th0, th1=new_th1))
     print(utils.estimate_price(new_th0, new_th1, 60949))
     
-    plt.plot(X, Y, 'ro')
+    figure, axis = plt.subplots(1, 2)
+
+    line_x = [min(X), max(X)]
+    line_y = [(new_th1 * i) + new_th0 for i in line_x]
+    axis[0].plot(line_x, line_y, 'b')
+    axis[0].plot(X, Y, 'ro')
+
+    axis[1].plot(cost)
     plt.show()
+
+    
         
     return 0
 
